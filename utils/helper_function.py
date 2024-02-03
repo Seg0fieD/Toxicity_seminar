487abf3c02cdcec784b94692cc5426ca13f34a27
Soch00001 <soch00001@uni-saarland.de>
2024-02-03 20:47:23 +0100

    updating Mordred fingerprint technique

--- utils/helper_function.py
+++ utils/helper_function.py
@@ -4,7 +4,9 @@ import os
 from rdkit import Chem
 from rdkit.Chem import MACCSkeys, rdFingerprintGenerator
 from rdkit import DataStructs
+import mordred
 from mordred import Calculator, descriptors
+from tqdm import tqdm
 
 
 
@@ -191,4 +193,40 @@ def valid_indices(smiles):
             valid_idx.append(i)
     
     return valid_idx
+
+
+
+
+
+
+class mordredWrapper:
+    def __init__(self, smiles_array) -> None:
+        self.calc = Calculator(descriptors, ignore_3D=True)
+
+        mol_list = []
+        for smiles in smiles_array:
+            mol = Chem.MolFromSmiles(smiles)
+            if mol is not None:
+                mol_list.append(mol)
+
+        df_mordred = self.calc.pandas(mol_list)
+        truth_map = df_mordred.applymap(lambda x : not isinstance(x, mordred.error.MissingValueBase))
+        truth_series = truth_map.all(axis=0)
+        self.mask = truth_series.to_numpy()
+
+    def get_fingerprints(self, smiles_array, labels):
+        fps = []
+        y = []
+        for smiles, label in zip(smiles_array, labels):
+            mol = Chem.MolFromSmiles(smiles)
+            if mol is None:
+                pass
+            else:
+                fps.append(np.array(self.calc(mol))[self.mask])
+                y.append(label)
+
+        assert len(fps) == len(y)
+        
+        return np.array(fps), np.array(y)
+
             
